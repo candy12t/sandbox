4849302e6c60a3ab992faf93cb93aa3e05fc982e
Takumi Kanada <candy12t@gmail.com>
2022-06-05 20:43:41 +0900

    Update pluralize

--- ruby/pluralize/test/pluralize_test.rb
+++ ruby/pluralize/test/pluralize_test.rb
@@ -3,14 +3,24 @@ require './lib/pluralize'
 
 class PluralizeTest < MiniTest::Test
   def test_pluralize
-    assert_equal 'buses', 'bus'.pluralize
-    assert_equal 'dishes', 'dish'.pluralize
+    assert_equal 'buses',    'bus'.pluralize
+    assert_equal 'dishes',   'dish'.pluralize
     assert_equal 'churches', 'church'.pluralize
-    assert_equal 'boxes', 'box'.pluralize
+    assert_equal 'boxes',    'box'.pluralize
     assert_equal 'tomatoes', 'tomato'.pluralize
-    assert_equal 'cities', 'city'.pluralize
-    assert_equal 'shelves', 'shelf'.pluralize
-    assert_equal 'knives', 'knife'.pluralize
-    assert_equal 'cats', 'cat'.pluralize
+    assert_equal 'cities',   'city'.pluralize
+    assert_equal 'shelves',  'shelf'.pluralize
+    assert_equal 'knives',   'knife'.pluralize
+    assert_equal 'cats',     'cat'.pluralize
+  end
+
+  def test_pluralize!
+    cat = 'cat'
+    cat.pluralize
+    assert_equal 'cat', cat
+
+    bus = 'bus'
+    bus.pluralize!
+    assert_equal 'buses', bus
   end
 end
