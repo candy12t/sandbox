eeee46864d98c3c938f9c180e85375ebdc2e12af
Takumi Kanada <candy12t@gmail.com>
2024-02-03 15:43:12 +0900

    Update: encoding/binary

--- golang/encoding/binary/main.go
+++ golang/encoding/binary/main.go
@@ -1,32 +1,115 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"encoding/binary"
 	"fmt"
 	"log"
+	"os"
+	"sync"
 )
 
 func main() {
-	b := []byte("hello world")
-	buf := &bytes.Buffer{}
-	if err := binary.Write(buf, binary.BigEndian, uint64(len(b))); err != nil {
-		log.Fatal(err)
-	}
-
-	_, err := buf.Write(b)
+	f, err := os.Create("test.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	f.Close()
 
-	p := make([]byte, 8)
-	if _, err := buf.Read(p); err != nil {
-		log.Fatal(err)
+	s, _ := newStore(f)
+	s.Append([]byte("hello world"))
+
+	var pos uint64
+	read, _ := s.Read(pos)
+	fmt.Println(read)
+}
+
+var (
+	enc = binary.BigEndian
+)
+
+const (
+	lenWidth = 8 // uint64 size
+)
+
+type store struct {
+	*os.File
+	mu   sync.Mutex
+	buf  *bufio.Writer
+	size uint64
+}
+
+func newStore(f *os.File) (*store, error) {
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
 	}
 
-	pp := make([]byte, binary.BigEndian.Uint64(p))
-	if _, err := buf.Read(pp); err != nil {
-		log.Fatal(err)
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		size: size,
+		buf:  bufio.NewWriter(f),
+	}, nil
+}
+
+func (s *store) Append(p []byte) (uint64, uint64, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	pos := s.size
+
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
 	}
-	fmt.Println(string(pp))
+
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	w += lenWidth
+	s.size += uint64(w)
+	return uint64(w), pos, nil
+}
+
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+	return s.File.ReadAt(p, off)
+}
+
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.buf.Flush(); err != nil {
+		return err
+	}
+
+	return s.File.Close()
 }
